e9d8395a16af1084476cb41acf04ac67ffbaf0a0
andglobal <41041064+andglobal@users.noreply.github.com>
2020-01-09 13:54:05 +0800

    update
    update

---
+++ js/player.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){var $videoSrc;$('.video-btn').click(function(){$videoSrc=$(this).data("src");});console.log($videoSrc);$('#myModal').on('shown.bs.modal',function(e){$("#video").attr('src',$videoSrc+"?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;autoplay=1");})
+$('#myModal').on('hide.bs.modal',function(e){$("#video").attr('src',$videoSrc);})});
